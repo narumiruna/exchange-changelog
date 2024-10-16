c6f311436b35f199ff04bc9912e7b9e6edd834e5
narumi <toucans-cutouts0f@icloud.com>
2024-10-16 22:19:50 +0800

    refactor

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ from exchange_changelog.config import load_config
 from exchange_changelog.loaders import load_html_with_httpx
 from exchange_changelog.loaders import load_html_with_singlefile
 from exchange_changelog.slack import post_slack_message
-from exchange_changelog.tools.changelog import ChangelogList
+from exchange_changelog.tools.changelog import Changelog
 from exchange_changelog.tools.changelog import extract_changelog
-from exchange_changelog.tools.changelog import select_recent_changelogs
 
 
 def load_html(url: str, method: Literal["httpx", "singlefile"]) -> str:
@@ -26,25 +25,26 @@ def load_html(url: str, method: Literal["httpx", "singlefile"]) -> str:
         raise ValueError(f"unknown method: {method}")
 
 
-def extract_recent_changelog(api_doc: APIDoc, cfg: Config) -> ChangelogList:
+def extract_recent_changelog(api_doc: APIDoc, cfg: Config) -> Changelog:
     text = load_html(api_doc.url, api_doc.method)
     logger.info("text length: {}", len(text))
 
     # trim text
     text = text[: cfg.trim_len]
 
-    changelogs = extract_changelog(text)
-    if changelogs is None:
-        logger.info("no changelogs found for {}", api_doc.name)
-        return ""
+    try:
+        changelog = extract_changelog(text)
+    except Exception as e:
+        logger.error("unable to extract changelog: {}", e)
+        return Changelog(changes=[], upcoming_changes=[])
 
-    # log parsed changelogs
-    for changelog in changelogs.items:
-        logger.info("changelog: {}", changelog)
+    # log parsed changes
+    for change in changelog.changes:
+        logger.info("change: {}", change)
 
-    recent_changelogs = select_recent_changelogs(changelogs, cfg.num_days)
+    changelog.select_recent_changes(cfg.num_days)
 
-    return recent_changelogs
+    return changelog
 
 
 @click.command()
@@ -60,7 +60,7 @@ def main(config_file: Path, output_file: Path) -> None:
     for doc in cfg.docs:
         changelogs = extract_recent_changelog(doc, cfg)
 
-        if changelogs.items:
+        if changelogs.changes:
             logger.debug("info:\n{}", changelogs.pritty_repr(doc.name, doc.url))
             post_slack_message(changelogs.pritty_repr(doc.name, doc.url))
 
