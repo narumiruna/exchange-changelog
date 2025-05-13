80147a46d537a2d4e5579d1f6ab31784dc9f7303
narumi <toucans-cutouts0f@icloud.com>
2025-05-13 20:15:55 +0800

    use RichHandler

--- src/exchange_changelog/lazy.py
+++ src/exchange_changelog/lazy.py
@@ -21,19 +21,11 @@ T = TypeVar("T")
 
 @cache
 def get_openai_client() -> AsyncOpenAI:
-    # OpenAI-compatible endpoints
-    openai_proxy_api_key = os.getenv("OPENAI_PROXY_API_KEY")
-    openai_proxy_base_url = os.getenv("OPENAI_PROXY_BASE_URL")
-    if openai_proxy_api_key:
-        logger.info("Using OpenAI proxy API key")
-        return AsyncOpenAI(base_url=openai_proxy_base_url, api_key=openai_proxy_api_key)
-
     # Azure OpenAI-comatible endpoints
     azure_api_key = os.getenv("AZURE_OPENAI_API_KEY")
-    azure_openai_api_version = os.getenv("AZURE_OPENAI_API_VERSION", "2024-12-01-preview")
     if azure_api_key:
         logger.info("Using Azure OpenAI API key")
-        return AsyncAzureOpenAI(api_key=azure_api_key, api_version=azure_openai_api_version)
+        return AsyncAzureOpenAI(api_key=azure_api_key)
 
     logger.info("Using OpenAI API key")
     return AsyncOpenAI()
